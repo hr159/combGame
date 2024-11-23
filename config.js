ab02d3581bf56da60f80051a3aa062c6710b8acd
jokerjt <782311399@qq.com>
2024-11-23 21:44:40 +0800

    优化界面

--- config.js
+++ config.js
@@ -1,41 +1,69 @@
 const config = {
-    contractAddress: "0x700b6A60ce7EaaEA56F065753d8dcB9653dbAD35", // 替换为您的合约地址
+    contractAddress: "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720", // 替换为您的合约地址
     abi: [
-        {
-            "inputs": [],
-            "name": "getGameState",
-            "outputs": [
-                { "internalType": "uint256", "name": "", "type": "uint256" },
-                { "internalType": "uint8[4][4]", "name": "", "type": "uint8[4][4]" }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
-            "name": "playerGames",
-            "outputs": [{ "internalType": "uint256", "name": "score", "type": "uint256" }],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [],
-            "name": "startGame",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                { "internalType": "uint256", "name": "score", "type": "uint256" },
-                { "internalType": "uint8[4][4]", "name": "board", "type": "uint8[4][4]" }
-            ],
-            "name": "updateGame", // 确保这里有函数名
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        }
-    ]
+                 {
+                     "type": "function",
+                     "name": "getGameState",
+                     "inputs": [],
+                     "outputs": [
+                         {
+                             "name": "",
+                             "type": "uint256",
+                             "internalType": "uint256"
+                         },
+                         {
+                             "name": "",
+                             "type": "uint8[4][4]",
+                             "internalType": "uint8[4][4]"
+                         }
+                     ],
+                     "stateMutability": "view"
+                 },
+                 {
+                     "type": "function",
+                     "name": "playerGames",
+                     "inputs": [
+                         {
+                             "name": "",
+                             "type": "address",
+                             "internalType": "address"
+                         }
+                     ],
+                     "outputs": [
+                         {
+                             "name": "score",
+                             "type": "uint256",
+                             "internalType": "uint256"
+                         }
+                     ],
+                     "stateMutability": "view"
+                 },
+                 {
+                     "type": "function",
+                     "name": "startGame",
+                     "inputs": [],
+                     "outputs": [],
+                     "stateMutability": "nonpayable"
+                 },
+                 {
+                     "type": "function",
+                     "name": "updateGame",
+                     "inputs": [
+                         {
+                             "name": "score",
+                             "type": "uint256",
+                             "internalType": "uint256"
+                         },
+                         {
+                             "name": "board",
+                             "type": "uint8[4][4]",
+                             "internalType": "uint8[4][4]"
+                         }
+                     ],
+                     "outputs": [],
+                     "stateMutability": "nonpayable"
+                 }
+             ]
 };
 
 export default config;
